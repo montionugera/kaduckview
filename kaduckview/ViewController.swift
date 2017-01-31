c774331548340b8794f2d838becfa561c9a5ec7c
pasit nusso <montionugera@gmail.com>
2017-01-31 20:56:56 +0700

    init

---
+++ kaduckview/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  kaduckview
+//
+//  Created by montionugera on 1/31/17.
+//  Copyright © 2017 augimon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let fantasticView = KaduckView(frame: self.view.bounds)
+        
+        self.view.addSubview(fantasticView)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
